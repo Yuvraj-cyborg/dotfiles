97ed6d2b5c883cf97e51e4703c0163af263e29eb
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-10-31 23:47:27 +0530

    feat: added supermaven plugin for autocomplete

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,6 +5,42 @@ return {
     opts = require "configs.conform",
   },
 
+  -- Supermaven inline AI completions
+  {
+    "supermaven-inc/supermaven-nvim",
+    -- make commands available and load plugin when they are executed
+    cmd = {
+      "SupermavenStart",
+      "SupermavenStop",
+      "SupermavenRestart",
+      "SupermavenToggle",
+      "SupermavenStatus",
+      "SupermavenUseFree",
+      "SupermavenUsePro",
+      "SupermavenLogout",
+      "SupermavenShowLog",
+      "SupermavenClearLog",
+    },
+    event = "InsertEnter",
+    config = function()
+      require("supermaven-nvim").setup({
+        -- set to true to rely on nvim-cmp only
+        disable_inline_completion = false,
+        -- set keymaps if you want to override defaults
+        -- keymaps = { accept_suggestion = "<Tab>", clear_suggestion = "<C-]>", accept_word = "<C-j>" },
+      })
+    end,
+  },
+
+  -- add Supermaven source to nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, 1, { name = "supermaven" })
+    end,
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
